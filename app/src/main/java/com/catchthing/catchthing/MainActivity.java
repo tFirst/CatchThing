bb1cd547f772613b39b2dce71f95f3a5a604633e
tFirst <valtario349@gmail.com>
2017-12-20 01:46:11 +0700

    Фиксы

--- app/src/main/java/com/catchthing/catchthing/MainActivity.java
+++ app/src/main/java/com/catchthing/catchthing/MainActivity.java
@@ -4,15 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import com.catchthing.catchthing.games.GameLeft;
 import com.catchthing.catchthing.games.GameRight;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +27,5 @@ public class MainActivity extends AppCompatActivity {
     public void goGameRight(View view) {
         Intent intent = new Intent(this, GameRight.class);
         startActivity(intent);
-    }
-
-    @Override
-    public void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
     }
 }
